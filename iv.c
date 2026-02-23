2fe3bf12f04150adc67526bb47ee542a505dac30
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-23 14:24:42 +0530

    feat(iv): add CLI support and fix event loop

--- iv.c
+++ iv.c
@@ -1,13 +1,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_stdinc.h>
-#include <SDL2/SDL_surface.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  const int width = 800, height = 900;
+int main(int argc, char *argv[]) {
+  const char *filepath = (argc > 1) ? argv[1] : "/home/shreyanshxyz/Downloads/i.jpg";
 
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     fprintf(stderr, "SDL_Init error: %s\n", SDL_GetError());
@@ -22,17 +19,9 @@ int main() {
 
   SDL_Window *pwindow =
       SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, width, height, 0);
+                       SDL_WINDOWPOS_CENTERED, 800, 900, 0);
 
-  SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    if (event.type == SDL_QUIT) {
-      SDL_Quit();
-      exit(0);
-    }
-  }
-  SDL_Surface *image = IMG_Load("/home/shreyanshxyz/Downloads/i.jpg");
+  SDL_Surface *image = IMG_Load(filepath);
   if (image == NULL) {
     fprintf(stderr, "IMG_Load error: %s\n", IMG_GetError());
     IMG_Quit();
@@ -41,11 +30,19 @@ int main() {
   }
 
   SDL_SetWindowSize(pwindow, image->w, image->h);
-  psurface = SDL_GetWindowSurface(pwindow);
+  SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
   SDL_BlitSurface(image, NULL, psurface, NULL);
   SDL_UpdateWindowSurface(pwindow);
 
-  SDL_Delay(5000);
+  SDL_Event event;
+  int running = 1;
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        running = 0;
+      }
+    }
+  }
 
   SDL_FreeSurface(image);
   IMG_Quit();

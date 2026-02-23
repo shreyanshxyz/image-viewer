3dfcd7419f9f1c2cdc57ab4fbe87f8e8c1151961
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-23 14:12:25 +0530

    feat: add image loading support using SDL_image

--- iv.c
+++ iv.c
@@ -1,10 +1,10 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
   const int width = 800, height = 900;
@@ -13,27 +13,38 @@ int main() {
     fprintf(stderr, "SDL_Init error: %s\n", SDL_GetError());
     return 1;
   }
+
+  if (!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG)) {
+    fprintf(stderr, "IMG_Init error: %s\n", IMG_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
   SDL_Window *pwindow =
       SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, width, height, 0);
 
   SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
-
-  Uint8 r, g, b;
-  r = 0xFF;
-  g = b = 0;
-  Uint32 color = SDL_MapRGB(psurface->format, r, g, b);
-
-  SDL_Rect pixel = (SDL_Rect){0, 0, 1, 1};
-  for (int x = 0; x < width; x++) {
-    for (int y = 0; y < height; y++) {
-      pixel.x = x;
-      pixel.y = y;
-      SDL_FillRect(psurface, &pixel, color);
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    if (event.type == SDL_QUIT) {
+      SDL_Quit();
+      exit(0);
     }
   }
-
+  SDL_Surface *image = IMG_Load("/home/shreyanshxyz/Downloads/i.jpg");
+  if (image == NULL) {
+    fprintf(stderr, "IMG_Load error: %s\n", IMG_GetError());
+    IMG_Quit();
+    SDL_Quit();
+    return 1;
+  }
+  SDL_BlitSurface(image, NULL, psurface, NULL);
   SDL_UpdateWindowSurface(pwindow);
 
-  SDL_Delay(1000);
+  SDL_Delay(5000);
+
+  SDL_FreeSurface(image);
+  IMG_Quit();
+  SDL_Quit();
 }

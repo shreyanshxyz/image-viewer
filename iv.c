3f788c29166a13883329a211ba604d4fcb022812
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-23 13:03:59 +0530

    refactor(iv): use SDL window surface API

--- iv.c
+++ iv.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
@@ -14,30 +15,11 @@ int main() {
   SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED,
                                          SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 
-  printf("Video driver: %s\n", SDL_GetCurrentVideoDriver());
+  SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(pwindow, -1, 0);
-  if (renderer) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-  }
+  SDL_FillRect(psurface, NULL, 0xFF0000);
 
-  Uint32 start = SDL_GetTicks();
-  SDL_Event e;
-  while (SDL_GetTicks() - start < 3000) {
-    while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT) {
-        goto done;
-      }
-    }
-    SDL_Delay(16);
-  }
+  SDL_UpdateWindowSurface(pwindow);
 
-done:
-  if (renderer)
-    SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(pwindow);
-  SDL_Quit();
-  return 0;
+  SDL_Delay(1000);
 }

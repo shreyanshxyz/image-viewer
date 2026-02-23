146048f6ccf3327c343fa9b301ad196e797dbea8
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-23 12:25:34 +0530

    feat: create SDL window

--- iv.c
+++ iv.c
@@ -1,8 +1,11 @@
+#include <SDL2/SDL_video.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<SDL2/SDL.h>
 
 int main() {
   printf("Hello World!\n");
+
+  SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 }
 
